c9502947cddda58af614ea02220d980070f670d2
Davit Gelashvili <datoogelashvili@gmail.com>
2020-04-28 20:22:27 +0400

    upload

--- js/main.js
+++ js/main.js
@@ -1,36 +1,50 @@
 var objectkey;
 var objectval;
 
-document.getElementById("fileUpload").addEventListener("change", function(event) {
-    selectedFile = event.target.files[0];
-});
-document.getElementById("uploadExcel").addEventListener("click", function() {
-    if (selectedFile) {
-      var fileReader = new FileReader();
-      fileReader.onload = function(event) {
-        var data = event.target.result;
+/* set up XMLHttpRequest */
+var url = "base.xls";
+var oReq = new XMLHttpRequest();
+oReq.open("GET", url, true);
+oReq.responseType = "arraybuffer";
 
-        var workbook = XLSX.read(data, {
-          type: "binary"
-        });
-        // console.log(workbook)
-        workbook.SheetNames.forEach((sheet, index) => {
-          let rowObject = XLSX.utils.sheet_to_row_object_array(
-            workbook.Sheets[sheet]
-          );
-          $('.select').append(`
-            <select id="${index}">
-                <option value="">${sheet}</option>
-            </select>
-          `)
-          rowObject.map((item)=>{
-            insert(item, index)
-          })
-        });
-      };
-      fileReader.readAsBinaryString(selectedFile);
-    }
+oReq.onload = function(e) {
+  var arraybuffer = oReq.response;
+
+  /* convert data to binary string */
+  var data = new Uint8Array(arraybuffer);
+  var arr = new Array();
+  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+  var bstr = arr.join("");
+
+  /* Call XLSX */
+  var workbook = XLSX.read(bstr, {type:"binary"});
+
+
+workbook.SheetNames.forEach((sheet, index) => {
+  let rowObject = XLSX.utils.sheet_to_row_object_array(
+    workbook.Sheets[sheet]
+  );
+  $('.select').append(`
+    <select id="${index}">
+        <option value="">${sheet}</option>
+    </select>
+  `)
+
+//   console.log(rowObject)
+  rowObject.map((item)=>{
+    insert(item, index)
+    console.log(item, index)
+  })
 });
+
+  /* DO SOMETHING WITH workbook HERE */
+  var first_sheet_name = workbook.SheetNames[0];
+  /* Get worksheet */
+  var worksheet = workbook.Sheets[first_sheet_name];
+//   console.log(workbook);
+}
+
+oReq.send();
 
 function insert(item, index){
   objectkey = Object.keys(item)
@@ -40,4 +54,3 @@ function insert(item, index){
     <option>${item[objectkey]}<option>
   `)
 }
-  
